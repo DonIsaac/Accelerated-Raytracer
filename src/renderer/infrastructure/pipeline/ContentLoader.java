1b6bae12639fd6486563d9ccf8b2c6428df225a5
Don Isaac <donald.isaac@gmail.com>
2017-10-05 12:07:25 -0400

    I'm not totally sure at this point

---
+++ src/renderer/infrastructure/pipeline/ContentLoader.java
@@ -0,0 +1,16 @@
+package renderer.infrastructure.pipeline;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import renderer.scene.actors.Model;
+
+public class ContentLoader {
+	public ArrayList<Model> models;
+	
+	public Model loadModelFromObj(File objFile) {
+	
+		return new Model();
+	}
+	
+}
